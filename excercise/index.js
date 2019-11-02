3d572a815fa551cfda27b16fb7c8a218f674d057
mi-ko-cute <thx-mail@worysolve.com>
2019-11-02 16:30:08 +0900

    fetchライブラリーでクイズデータを取得する

---
+++ excercise/index.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+async function getQuiz() {
+    try {
+        const response = await axios.get('https://opentdb.com/api.php?amount=10');
+        const quizData = await response.data.results;
+        console.log(quizData);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getQuiz();
